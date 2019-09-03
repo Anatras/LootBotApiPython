999b724281a6b154c3790f68230d9414c9d17617
Anatras <50227010+Anatras@users.noreply.github.com>
2019-09-04 01:40:31 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ except ImportError:
 setup(
   name = 'LootGameBotApi',         # How you named your package folder (MyLib)
   packages = ['C:\\Users\\uni\\Desktop\\ApiLoot'],   # Chose the same as "name"
-  version = '0.13',      # Start with a small number and increase it with every change you make
+  version = '0.14',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python wrapper per le API di LootBot',   # Give a short description about your library
   author = 'Alessio Balderi',                   # Type in your name
   author_email = 'balderialessio@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Anatras',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Anatras/LootBotApiPython/archive/0.12.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Anatras/LootBotApiPython/archive/0.13.tar.gz',    # I explain this later on
   keywords = ['LootBot', 'API', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
